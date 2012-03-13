6dac81048b52a46c3a1d745ddd0a3805b0d0526b
Seitarou <fg2qcp@gmail.com>
2012-03-13 23:36:46 +0900

    [未確認] relike(unlike->like) 機能を実装しました。

---
+++ python/xauth_client.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urlparse
+import oauth2 as oauth
+import simplejson
+import urllib
+import netrc
+import re
+import ConfigParser
+from argparse import ArgumentParser
+
+usage = "usage: %prog [options]"
+
+consumer_key, consumer_secret = None, None
+xauth_token, xauth_token_secret = None, None
+
+ini = ConfigParser.SafeConfigParser()
+if ini.read('xauth.ini'):
+    if ini.has_option('consumer', 'key'):
+        consumer_key = ini.get('consumer', 'key')
+    if ini.has_option('consumer', 'secret'):
+        consumer_secret = ini.get('consumer', 'secret')
+    if ini.has_option('xauth', 'token'):
+        xauth_token = ini.get('xauth', 'token')
+    if ini.has_option('xauth', 'token_secret'):
+        xauth_token_secret = ini.get('xauth', 'token_secret')
+else:
+    nrc = netrc.netrc()
+    consumer_key, _, consumer_secret = nrc.authenticators('tumblr_consumer')
+    xauth_token, _, xauth_token_secret = nrc.authenticators('tumblr_xauth_token')
+
+
+class Post(object):
+
+    @staticmethod
+    def parse(parent, json):
+        post = {
+            'text': Text,
+            'photo': Photo,
+            'quote': Quote,
+            'link': Link,
+            'answer': Chat,  # checking name
+            'audio': Audio,
+            'video': Video
+        }[json['type']](parent, json)
+
+        # post.data['state']
+        post.data['tags'] = json['tags']
+        post.id = json['id']
+        post.reblog_key = json['reblog_key']
+        post.post_url = json['post_url']
+        # post.data['tweet']
+        # post.data['date']
+        # post.data['slug']
+
+        return post
+
+    def __init__(self, parent):
+        self.data = {}
+        self.tumblelog = parent
+
+    def publish(self):
+        pass
+
+    def like(self):
+        client = build_oauth_client()
+        url = 'http://api.tumblr.com/v2/user/like'
+        resp, content = client.request(url, method='POST', body='id=%d&reblog_key=%s' % (self.id, self.reblog_key))
+
+        print content
+
+        json = simplejson.loads(content)
+        if json['meta']['msg'] == 'OK':
+            return True
+        elif json['meta']['status'] == "404":
+            # reblog key が使用できない場合は 404 が帰ってきます
+            return False
+        return False
+
+    def unlike(self):
+        client = build_oauth_client()
+        url = 'http://api.tumblr.com/v2/user/unlike'
+        resp, content = client.request(url, method='POST', body='id=%d&reblog_key=%s' % (self.id, self.reblog_key))
+
+        print content
+
+        json = simplejson.loads(content)
+        if json['meta']['msg'] == 'OK':
+            return True
+        return False
+
+
+class Text(Post):
+    def __init__(self, tumblelog, json):
+        super(Text, self).__init__(tumblelog)
+
+        self.data['type'] = 'text'
+
+        alias = {'title': 'title', 'body': 'body'}
+        self.data.update(pickup_aliases(json, alias))
+
+
+class Photo(Post):
+    def __init__(self, tumblelog, json):
+        super(Photo, self).__init__(tumblelog)
+
+        self.data['type'] = 'photo'
+
+        # photo set にも対応させる
+        alias = {'caption': 'caption', 'link_url': 'link'}
+        self.data.update(pickup_aliases(json, alias))
+
+
+class Quote(Post):
+    def __init__(self, tumblelog, json):
+        super(Quote, self).__init__(tumblelog)
+
+        self.data['type'] = 'quote'
+
+        alias = {'text': 'quote', 'source': 'source'}
+        self.data.update(pickup_aliases(json, alias))
+
+
+class Link(Post):
+    def __init__(self, tumblelog, json):
+        super(Link, self).__init__(tumblelog)
+
+        self.data['type'] = 'link'
+
+        alias = {'title': 'title', 'url': 'url', 'description': 'description'}
+        self.data.update(pickup_aliases(json, alias))
+
+
+class Chat(Post):
+    def __init__(self, tumblelog, json):
+        super(Chat, self).__init__(tumblelog)
+
+        self.data['type'] = 'chat'
+
+        alias = {'title': 'title', 'conversation': 'body'}
+        self.data.update(pickup_aliases(json, alias))
+
+
+class Audio(Post):
+    def __init__(self, tumblelog, json):
+        super(Audio, self).__init__(tumblelog)
+
+        self.data['type'] = 'audio'
+
+        alias = {'caption': 'caption'}
+        self.data.update(pickup_aliases(json, alias))
+        # self.data['external_url']
+
+
+class Video(Post):
+    def __init__(self, tumblelog, json):
+        super(Video, self).__init__(tumblelog)
+
+        self.data['type'] = 'video'
+
+        alias = {'caption': 'caption'}
+        self.data.update(pickup_aliases(json, alias))
+
+
+class Tumblelog(object):
+
+    def __init__(self, tumblelog):
+        self.name = tumblelog
+        self.posts = []
+        self.content_log = content_log
+
+    def info(self, tumblelog=None):
+        client = build_oauth_client()
+        if tumblelog == None:
+            tumblelog = self.name
+        url = "http://api.tumblr.com/v2/blog/%s/info?api_key=%s" % (tumblelog, consumer_key)
+        resp, content = client.request(url, method='GET')
+
+        self.content = content
+        self.json = simplejson.loads(content)
+
+        self.msg = self.json['meta']['msg']
+        self.status = self.json['meta']['status']
+
+        return self.json
+
+    def getpost(self, post_url):
+        client = build_oauth_client()
+        m = re.match('http://([^/]+)/post/(\d+)', post_url)
+        tumblelog = m.group(1)
+        post_id = m.group(2)
+        url = "http://api.tumblr.com/v2/blog/%s/posts?api_key=%s&id=%s" % (
+            tumblelog, consumer_key, post_id)
+        resp, content = client.request(url, method='GET')
+
+        self.content = content
+        self.json = simplejson.loads(content)
+
+        self.msg = self.json['meta']['msg']
+        self.status = self.json['meta']['status']
+
+        self.post = Post.parse(self, self.json['response']['posts'][0])
+
+        return self.posts
+
+    def likes(self, offset=0, limit=20):
+        client = build_oauth_client()
+        url = "http://api.tumblr.com/v2/user/likes?offset=%d&limit=%d" % (offset, limit)
+        resp, content = client.request(url, method='GET')
+
+        self.content = content
+        self.json = simplejson.loads(content)
+
+        self.msg = self.json['meta']['msg']
+        self.status = self.json['meta']['status']
+
+        self.liked_count = self.json['response']['liked_count']
+
+        self.posts = []
+        for post in self.json['response']['liked_posts']:
+            self.posts.append(Post.parse(self, post))
+
+        return self.posts
+
+    def drafts(self):
+        client = build_oauth_client()
+        url = 'http://api.tumblr.com/v2/blog/%s/posts/draft' % (self.name)
+        resp, content = client.request(url, method='GET')
+
+        self.content = content
+        self.json = simplejson.loads(content)
+
+        self.msg = self.json['meta']['msg']
+        self.status = self.json['meta']['status']
+
+        self.posts = []
+        for post in self.json['response']['posts']:
+            self.posts.append(Post.parse(self, post))
+
+
+def posts_from_content(content):
+    json = simplejson.loads(content)
+    posts = []
+    if 'posts' in self.json['response']:
+        for post in json['response']['posts']:
+            posts.append(Post.parse(self, post))
+    elif 'liked_posts' in self.json['response']:
+        for post in json['response']['liked_posts']:
+            posts.append(Post.parse(self, post))
+    return posts
+
+
+def build_oauth_client():
+    consumer = oauth.Consumer(consumer_key, consumer_secret)
+    token = oauth.Token(xauth_token, xauth_token_secret)
+    client = oauth.Client(consumer, token)
+    client.set_signature_method = oauth.SignatureMethod_HMAC_SHA1()
+    return client
+    # この関数で使うかも知れないし使わないかも知れない
+    # params['x_auth_mode'] = 'client_auth'
+    # params['oauth_version'] = '1.0a'
+
+
+def pickup_aliases(src, aliases):
+    results = {}
+    for key, value in aliases.iteritems():
+        if key in src:
+            results[value] = src[key]
+    return results
+
+
+def main():
+    parser = ArgumentParser()
+
+    choice_command = ['relike']
+    # git のような３項オペレータにする
+    # %prog get_option put_opition
+    # xauth_token の入力
+    parser.add_argument("fetch", dest="fetch", help=u"ポストの読み込みタイプ")
+    parser.add_argument("command", dest="command", choice=choice_command, help=u"読み込んだポストの処理法")
+    parser.add_argument("-t", "--tumblelog", dest="tumblelog", help=u"ターゲットのTumblelogを指定してください。")
+    parser.add_argument("-V", "--like-vacuum", dest="like_vacuum", action="store_true", default=False,
+                      help=u"Likes を全て Unlike にします。")
+    parser.add_argument("-O", "--save", dest="content_file", default=None,
+                      help=u"APIで取得したテキストを全て保存します。")
+
+    args = parser.parse_args()
+
+    if not args.tumblelog:
+        tumblelog = raw_input('Input tumblelog: ')
+        t = Tumblelog(tumblelog)
+    else:
+        t = Tumblelog(args.tumblelog)
+
+    # 特殊なオプションの場合
+    if args.second == 'relike':
+        tempfile = __import__('tempfile')
+        fn = tempfile.mktemp()
+        f = open(fn, 'w')
+
+        t.likes(0, 1)
+        liked_count = t.liked_count
+
+        fail_unlikes = []  # unlike が必ず失敗するポストが溜まって無限ループに陥るのを防ぎます
+        for i in xrange(0, (liked_count - 1 / 20.0) + 1):
+            posts = t.likes(len(fail_unlikes), 20)
+            for post in posts:
+                if not post.unlike():
+                    fail_unlikes.append(post)
+            f.write(t.content)
+            f.write('\n')
+        f.close()
+        del fail_unlikes
+
+        f = open(fn, 'r')
+        fail_likes = []  # like fail は致命的なため確保しておきます
+        for line in f:
+            posts = posts_from_content(line)
+            for post in posts:
+                if not post.like():
+                    fail_likes.append(post)
+        f.close()
+
+        print 'Failed likes:'
+        for post in fail_likes:
+            print post.post_url
+        return
+
+    # posts を取得する
+    if args.fetch == 'dashboard':
+        pass
+    elif args.fetch == 'posts':
+        pass
+    elif args.fetch == 'drafts':
+        pass
+    elif args.fetch == 'likes':
+        pass
+    else:
+        return
+
+    # posts を処理する
+    if args.command == 'like':
+        pass
+    elif args.command == 'publish':
+        pass
+    else:
+        return
+
+
+if __name__ == '__main__':
+    main()
