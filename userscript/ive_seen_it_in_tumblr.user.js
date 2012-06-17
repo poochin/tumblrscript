2fea51c339b9918607286311a74e53678987f2b6
Seitarou <fg2qcp@gmail.com>
2012-06-17 14:47:38 +0900

    一度見たポストを Dashboard に表示しないようにします

---
+++ userscript/ive_seen_it_in_tumblr.user.js
@@ -0,0 +1,138 @@
+// ==UserScript==
+// @name        I've seen it in tumblr.
+// @match       http://www.tumblr.com/dashboard*
+// @version     1.0.0
+// @description 一度見たポストを表示しないようにさせます
+// 
+// @author      poochin
+// @license     MIT
+// @updated     2012-06-14
+// ==/UserScript==
+
+
+/**
+ * @fixme 自分のポストに対しては動きません
+ */
+(function IveSeenItInTumblr() {
+    var reblog_keys,
+        enable;
+
+    boot();
+    
+    function postappended(post) {
+        var reblog_button, reblog_key;
+    
+        reblog_button = post.querySelector('a.reblog_button');
+        if (reblog_button) {
+            reblog_key = reblog_button.getAttribute('data-reblog-key');
+            if (reblog_keys[reblog_key]) {
+                console.log('Delete:', post.getAttribute('id'));
+                post.parentNode.removeChild(post);
+            }
+            else {
+                reblog_keys[reblog_key] = 1;
+            }
+        }
+    }
+    
+    function appendToPosts(e) {
+        if (enable == false) {
+            /* pass */
+        }
+        else if (e.target.nodeType == document.ELEMENT_NODE) {
+            postappended(e.target);
+        }
+    }
+    
+    function init() {
+        enable = localStorage.getItem('itintumblr_enable');
+        enable = (enable == null ? false : enable == 'true');
+    
+        reblog_keys = null;
+        if (enable) {
+            reblog_keys = localStorage.getItem('itintumblr_reblog_keys');
+            reblog_keys = (reblog_keys == null ? {} : JSON.parse(reblog_keys));
+        }
+    }
+    
+    function command_field() {
+        var base, right_column, itintumblr, toggle_messages;
+    
+        toggle_messages = ['toEnable', 'toDisable'];
+    
+        base = [
+            '<fieldset id="itintumblr" style="border-radius: 6px;">',
+            '<legend>I\'ve seen it in tumblr.</legend>',
+            '<button class="enable_toggle_button"></button>',
+            '<button class="check">Check</button>',
+            '<br />',
+            '<button class="save_button">Save</button>',
+            '<button class="clear_button">Clear</button>',
+            '</fieldset>'].join('');
+    
+        right_column = document.querySelector('#right_column');
+        right_column.innerHTML += base;
+    
+        itintumblr = right_column.querySelector('#itintumblr');
+    
+        itintumblr.querySelector('.enable_toggle_button').innerText = toggle_messages[enable + 0];
+        itintumblr.querySelector('.enable_toggle_button').addEventListener('click', function(e) {
+            enable = !enable;
+            localStorage.setItem('itintumblr_enable', enable);
+            e.target.innerText = toggle_messages[enable + 0];
+            if (enable && reblog_keys === null) {
+                reblog_keys = localStorage.getItem('itintumblr_reblog_keys');
+                reblog_keys = (reblog_keys == null ? {} : JSON.parse(reblog_keys));
+            }
+        }, false);
+    
+        itintumblr.querySelector('.check').addEventListener('click', function(e) {
+            var k, cnt;
+    
+            cnt = 0;
+            for (k in reblog_keys) {
+                cnt++;
+            }
+    
+            alert('閲覧済みのポスト数: ' + cnt + '個');
+        }, false);
+    
+        itintumblr.querySelector('.save_button').addEventListener('click', function(e) {
+            if (reblog_keys !== null) {
+                localStorage.setItem('itintumblr_reblog_keys', JSON.stringify(reblog_keys));
+            }
+        }, false);
+    
+        itintumblr.querySelector('.clear_button').addEventListener('click', function(e) {
+            if (confirm('記憶済みの post をクリアしますか？')) {
+                localStorage.removeItem('itintumblr_reblog_keys');
+                reblog_keys = {};
+            }
+        }, false);
+    }
+    
+    function main() {
+        init();
+        command_field();
+    
+        document.querySelector('#posts').addEventListener('DOMNodeInserted', appendToPosts, true);
+        if (enable) {
+            Array.prototype.slice.call(document.querySelectorAll('#posts > li.post:not(.new_post)')).map(postappended);
+        }
+    }
+    
+    function isExecPage() {
+        return /^https?:\/\/[^\/]+\/.*/.test(location);
+    }
+    
+    function boot() {
+        if (window.document.body) {
+            if (isExecPage() /* for Opera */) {
+                main();
+            }
+        }
+        else {
+            window.document.addEventListener('DOMContentLoaded', main, false);
+        }
+    }
+})();
