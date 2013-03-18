aef15fdb0d5ad8dcd0909c3fd82384090bd438c1
Seitarou <fg2qcp@gmail.com>
2013-03-19 08:32:33 +0900

    Endless Summer が動かなくなってしまっていたため修正しました

--- userscript/tumblr_tornado.user.js
+++ userscript/tumblr_tornado.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Tumblr Tornado
 // @namespace   https://github.com/poochin
-// @version     1.2.9.8
+// @version     1.2.9.10
 // @description Tumblr にショートカットを追加するユーザスクリプト
 // @include     http://www.tumblr.com/dashboard
 // @include     http://www.tumblr.com/dashboard?oauth_token=*
@@ -2113,7 +2113,7 @@
         '}',
         '(window.Tumblr) && (Tumblr.KeyCommands) && (Tumblr.KeyCommands.scroll_speed=20);',
         'window.ison_endless_summer = false;',
-        'window.endless_summer_first_post_id = parseInt($$("#posts>.post[data-post-id]")[0].getAttribute("data-post-id"));',
+        'window.endless_summer_first_post_id = parseInt(document.querySelector("#posts>.post[data-post-id]").getAttribute("data-post-id"));',
     ];
 
     Tornado.windows.tornado_config = function tornado_config(e) {
