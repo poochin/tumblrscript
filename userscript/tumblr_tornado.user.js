8fb3080cbc806b05cbf6481df565b28ece9b35db
Seitarou <fg2qcp@gmail.com>
2013-10-10 09:07:05 +0900

    Activity ページで Shift-R をした際に Activity ページから離れてしまう不具合を修正しました

--- userscript/tumblr_tornado.user.js
+++ userscript/tumblr_tornado.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Tumblr Tornado
 // @namespace   https://github.com/poochin
-// @version     1.2.9.45
+// @version     1.2.9.46
 // @description Tumblr にショートカットを追加するユーザスクリプト
 // @include     http://www.tumblr.com/dashboard
 // @include     http://www.tumblr.com/dashboard?oauth_token=*
