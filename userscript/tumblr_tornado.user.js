f4bdd94ef51d32e5df9c4846458b22a0a1bd298d
Seitarou <fg2qcp@gmail.com>
2013-10-12 16:48:54 +0900

    n キー(notes開閉)後に l キー(like/unlike)を押すと notes が開いてしまう不具合を修正しました

--- userscript/tumblr_tornado.user.js
+++ userscript/tumblr_tornado.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Tumblr Tornado
 // @namespace   https://github.com/poochin
-// @version     1.2.9.49
+// @version     1.2.9.50
 // @description Tumblr にショートカットを追加するユーザスクリプト
 // @include     http://www.tumblr.com/dashboard
 // @include     http://www.tumblr.com/dashboard?oauth_token=*
@@ -2393,7 +2393,7 @@
                 key_bind: ['l'],
                 // func: CustomFuncs.default,
                 func: function like(post) {
-                    post.querySelector('.like').dispatchEvent(Vals.left_click);
+                    post.querySelector('.like.post_control').dispatchEvent(Vals.left_click);
                 },
                 title: 'Like',
                 desc: {
