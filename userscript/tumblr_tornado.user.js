8d81cd3170d251dd10b807708c9f1ba117b8b4c9
Seitarou <fg2qcp@gmail.com>
2013-11-06 00:52:36 +0900

    test

--- userscript/tumblr_tornado.user.js
+++ userscript/tumblr_tornado.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Tumblr Tornado
 // @namespace   https://github.com/poochin
-// @version     1.2.9.53
+// @version     1.2.9.54
 // @description Tumblr にショートカットを追加するユーザスクリプト
 // @include     http://www.tumblr.com/dashboard
 // @include     http://www.tumblr.com/dashboard?oauth_token=*
