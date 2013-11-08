8b9cdce29605811e34351d2b0b882d31b0236684
Seitarou <fg2qcp@gmail.com>
2013-11-08 15:06:45 +0900

    Necromancy Tumblelog 形式でも動作するように対応しました

--- userscript/tumblr_tornado.user.js
+++ userscript/tumblr_tornado.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Tumblr Tornado
 // @namespace   https://github.com/poochin
-// @version     1.2.9.55
+// @version     1.2.9.56
 // @description Tumblr にショートカットを追加するユーザスクリプト
 // @include     http://www.tumblr.com/dashboard
 // @include     http://www.tumblr.com/dashboard?tumblelog
