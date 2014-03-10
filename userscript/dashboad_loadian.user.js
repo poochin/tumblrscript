3029cc470f01909c8be9a814e695370f4e90c666
poochin <fg2qcp@gmail.com>
2014-03-10 14:04:36 +0900

    スクロールしなくても先読みしてくれる機能を付けました（デフォルトでは機能はオフ）

--- userscript/dashboad_loadian.user.js
+++ userscript/dashboad_loadian.user.js
@@ -22,17 +22,21 @@
     boot();
 
     /**
+     * buildElement
      * Node を作成し各種データを同時にセットします
+     * 
      * @param {String} tag_name タグ名.
      * @param {Object} propaties 辞書型のデータ.
      * @param {String} HTML 文字列.
      * @return {Object} 作成した Node を返します.
      */
-    /* buildElement */
+
     function buildElement(tag_name, propaties, innerHTML) {
+
         var elm = document.createElement(tag_name);
     
         for (var key in (propaties || {})) {
+
             elm.setAttribute(key, propaties[key]);
         }
     
@@ -40,40 +44,83 @@
         return elm;
     }
 
+
     /**
      * クライアント領域で Script を実行します
      * @param {String} code 実行したいコード
      */
+
     function execScript(code) {
+
         var script = document.createElement('script');
+
         script.setAttribute('type', 'text/javascript');
         script.innerHTML = code;
+
         script.addEventListener('onload', function(e) {
-            (function letit(elm) {
-                elm.parentNode.removeChild(elm);
-            })(e.target);
+
+            var elm = e.target;
+            elm.parentNode.removeChild(elm);
         });
+
         document.body.appendChild(script);
     };
+
 
     /**
      * Dashboard で次ページ読み込みを開始するかどうかを判定する関数をラップします
      */
+
     function wrapperAutoPaginator() {
+
       var f = Tumblr.Events._events['DOMEventor:flatscroll'][1].callback;
-      Tumblr.Events._events['DOMEventor:flatscroll'][1].callback = function(n){
-        if (document.querySelector('[name=dashboard_loadian_on]').checked) {
+
+      Tumblr.Events._events['DOMEventor:flatscroll'][1].callback = function dashboardLoadian(n) {
+
+        if (document.querySelector('#dashboard_loadian_custom_form [name=dashboard_loadian_on]').checked) {
+
           var offset = parseInt(document.querySelector('#right_column [name=offset]').value);
           n = jQuery.extend({}, n, {windowScrollY: n.windowScrollY + (isNaN(offset) ? 0 : offset)});
+
         }
+
         f(n);
       };
     };
+
+    function dashboardLoadian_autoLoader() {
+
+        var n = {
+            documentHeight:  document.body.scrollHeight,
+            windowHeight:    document.documentElement.clientHeight,
+            windowWidth:     document.documentElement.clientWidth,
+            windowScrollTop: document.documentElement.scrollTop || document.body.scrollTop,
+            windowScrollY: document.documentElement.scrollTop || document.body.scrollTop
+        };
+
+        var f;
+
+        if (document.querySelector('#dashboard_loadian_custom_form [name=dashboard_loadian_auto]').checked) {
+
+            try {
+                f = Tumblr.Events._events['DOMEventor:flatscroll'].filter(function(o) {
+                    return o.callback.name == 'dashboardLoadian';
+                })[0].callback;
+
+                f(n);
+            }
+            catch (e) { console.error(e); }
+
+        }
+
+        setTimeout(dashboardLoadian_autoLoader, 1000);
+    }
 
     /**
      * 読み込みを開始するスクロール位置をカスタマイズするフォームを埋め込みます。
      */
     function embedCustomForm() {
+
         var right_column, form_html;
 
         form_html = [
@@ -81,10 +128,12 @@
             '<label><input type="checkbox" name="dashboard_loadian_on" checked>On</label>: ',
             '<input size="3" type="text" name="offset" value="2000"/>',
             'px(Offset)',
+            '<br />',
+            '<label><input type="checkbox" name="dashboard_loadian_auto">Auto Loader</label>',
             ].join('');
 
         loadian.form = buildElement('fieldset',
-            {id: 'dashboard_loadian_custom_form;',
+            {id: 'dashboard_loadian_custom_form',
              style: 'border-radius: 6px'},
             form_html);
 
@@ -92,29 +141,42 @@
         right_column.appendChild(loadian.form);
     }
 
-    /** main 関数です */
+
+    /**
+     * main 関数です
+     **/
+
     function main() {
+
         embedCustomForm();
+
         execScript('void ' + wrapperAutoPaginator + '()');
+        execScript(dashboardLoadian_autoLoader + '; dashboardLoadian_autoLoader()');
     }
+
 
     /**
      * 実行すべきページかどうかを判断します。
      * Opera 用のコードです。
+     *
      * @return {Bool} 実行すべきページなら true を返します
      */
     function isExecPage() {
+
         return /^https?:\/\/www\.tumblr\.com\/dashboard.*/.test(location) ||
                /^https?:\/\/www\.tumblr\.com\/show.*/.test(location) ||
                /^https?:\/\/www\.tumblr\.com\/likes.*/.test(location) ||
                /^https?:\/\/www\.tumblr\.com\/tagged.*/.test(location);
     }
 
+
     /**
      * 始めに呼び出される関数です。
      * 実行すべきページの場合は main 関数を呼びだすようにします。
      */
+
     function boot() {
+
         if (isExecPage() === false) {
             return;
         }
